fc8f9d68cac60b682e7281215225f292302e38aa
tayeumi <tayeumi@gmail.com>
2015-09-09 09:02:02 +0700

    v2

---
+++ OutSource/NW_Traffic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+namespace OutSource
+{
+    
+        public class NW_Traffic
+        {
+            public string MacAddress { get; set; }
+            public string Month { get; set; }
+            public string Year { get; set; }
+            public DataTable NW_Trafic_Getlike()
+            {
+                string procname = "NW_Trafic_Getlike";
+                DbAccess db = new DbAccess();
+                db.CreateNewSqlCommand();
+                db.AddParameter("@MacAddress", MacAddress);
+                db.AddParameter("@Month", Month);
+                db.AddParameter("@Year", Year);
+                return db.ExecuteDataTable(procname);
+            }
+        }
+    
+}
